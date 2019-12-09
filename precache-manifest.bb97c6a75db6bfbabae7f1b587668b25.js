1cac7ba1bbe0ae401349a2fd04da5d472795ef09
sabsrolz <sabsrolz@umich.edu>
2019-12-09 13:07:44 -0600

    Updates

---
+++ precache-manifest.bb97c6a75db6bfbabae7f1b587668b25.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff83db60a67bd63f44efd60db9d7e751",
+    "url": "/MemoryApp/index.html"
+  },
+  {
+    "revision": "6b5c421b3c0c31d1f20a",
+    "url": "/MemoryApp/static/js/2.944c0a69.chunk.js"
+  },
+  {
+    "revision": "c807e2f3c26dd431ca02",
+    "url": "/MemoryApp/static/js/main.fa4816f7.chunk.js"
+  },
+  {
+    "revision": "ea5e0be72faf8383a087",
+    "url": "/MemoryApp/static/js/runtime-main.a7614e72.js"
+  }
+]);
